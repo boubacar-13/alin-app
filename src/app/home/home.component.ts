6609996bcc0bcbc46df4a3f31d621a118ed8aa18
Boubacar <timera.boubacar@gmail.com>
2023-10-04 08:43:08 +0200

    Add filter in home, json server, form in details

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -15,15 +15,21 @@ import { HousingService } from '../housing.service';
           type="text"
           placeholder="Filter by city..."
           style="border: 1px black solid;"
+          #filter
         />
-        <button class="primary" type="button" style="background: mediumpurple;">
+        <button
+          class="primary"
+          type="button"
+          style="background: mediumpurple;"
+          (click)="filterResults(filter.value)"
+        >
           Chercher
         </button>
       </form>
     </section>
     <section class="results">
       <app-housing-location
-        *ngFor="let item of housingLocationList"
+        *ngFor="let item of filteredLocationList"
         [housingLocation]="item"
       ></app-housing-location>
     </section>
@@ -33,8 +39,24 @@ import { HousingService } from '../housing.service';
 export class HomeComponent {
   housingLocationList: HousingLocation[] = [];
   housingService: HousingService = inject(HousingService);
+  filteredLocationList: HousingLocation[] = [];
 
   constructor() {
-    this.housingLocationList = this.housingService.getAllHousingLocations();
+    this.housingService
+      .getAllHousingLocations()
+      .then((housingLocationList: HousingLocation[]) => {
+        this.housingLocationList = housingLocationList;
+        this.filteredLocationList = housingLocationList;
+      });
+  }
+
+  filterResults(text: string) {
+    if (!text) {
+      this.filteredLocationList = this.housingLocationList;
+    }
+
+    this.filteredLocationList = this.housingLocationList.filter((item) =>
+      item?.city.toLowerCase().includes(text.toLocaleLowerCase())
+    );
   }
 }
